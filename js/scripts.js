2f27ffd9d37e5aa3e1c41833af6f29366724c982
Eriqbush <157830464+Eriqbush@users.noreply.github.com>
2024-02-14 12:28:31 -0500

    recommitting
    trying to recommit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Eriq Bush UI Portfolio")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+    } else {
+    x.className = 'navtoggle';
+    }
+    }
